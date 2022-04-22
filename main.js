c837e069e90761638e41e7cf558b88c1525cc35e
Shrief Essam <shriefessam1999@gmail.com>
2022-04-22 23:25:40 +0200

    add operator function this function will take the user input and use the right mathematical function

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-add((a,b) => a+b);
-subtract((a,b) => a-b);
-multiply((a,b) => a*b);
-divide((a,b) => a/b);
+add = ((a,b) => a+b);
+subtract = ((a,b) => a-b);
+multiply = ((a,b) => a*b);
+divide = ((a,b) => a/b);
 
-
+function operate(number1,number2,operator){
+   if (operator == '+') return add(number1,number2);
+   if (operator == '-') return subtract(number1,number2);
+   if (operator == '*') return multiply(number1,number2);
+   if (operator == '/') return divide(number1,number2);
+}
 
