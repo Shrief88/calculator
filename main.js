4d89f5983e05844921eff6ad56666a393ccca4c4
Shrief Essam <shriefessam1999@gmail.com>
2022-04-23 05:00:03 +0200

    update how calculator will work

--- main.js
+++ main.js
@@ -10,39 +10,51 @@ function operate(number1,number2,operator){
    if (operator == '/') return divide(number1,number2);
 }
 
-
+function showResult(){
+   displayArea.textContent = operate(firstNumber,secondNumber,operator);
+   firstNumber = displayArea.textContent;
+   secondNumber='';
+}
 
 
 let isFirstNumber = true;
+
 const numberButtons = document.querySelectorAll('.numberButton');
 const operatorButtons = document.querySelectorAll('.operatorButton');
-const display =document.querySelector('#result')
-const equal = document.querySelector('#equal');
+const equation =document.querySelector('#equation')
+const displayArea =document.querySelector('#result')
+const equalButton = document.querySelector('#equal');
+const clearButton = document.querySelector('#clear');
+const deleteButton = document.querySelector('#delete');
+
+
 let firstNumber = '';
 let secondNumber = '';
 let operator = '';
 
 
 numberButtons.forEach(button=>{
-   button.addEventListener('click',()=>{
-      
-      isFirstNumber ? firstNumber += button.value : secondNumber +=button.value;    
-      display.textContent = isFirstNumber ? firstNumber : secondNumber;  
+   button.addEventListener('click',()=>{    
+      isFirstNumber ? firstNumber += button.value : secondNumber +=button.value;  
+      displayArea.textContent = isFirstNumber ? firstNumber : secondNumber; 
+      equation.textContent = `${firstNumber} ${operator}`
    })
 })
 
 operatorButtons.forEach(button=>{
    button.addEventListener('click',()=>{
       isFirstNumber = false;
+      if(secondNumber.length > 0){
+         showResult()
+      } 
       operator = button.value;
+      equation.textContent = `${firstNumber} ${operator}`
    })
 })
 
 equal.addEventListener('click',()=>{
-   display.textContent = operate(firstNumber,secondNumber,operator);
-   firstNumber = display.textContent;
-   secondNumber='';
+   equation.textContent = `${firstNumber} ${operator} ${secondNumber} = `   
+   showResult();
 })
-
 
 
